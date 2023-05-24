7ce11cda760f991aca1d7771c61804ff9ab6aa37
ramachandra.m <ramachandra.m@xcubelabs.com>
2023-05-24 18:23:00 +0530

    first commit

---
+++ src/main/java/com/nr/javatraining/JavaTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.nr.javatraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTrainingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nr/javatraining/controller/ApiController.java
@@ -0,0 +1,16 @@
+package com.nr.javatraining.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/nr")
+public class ApiController {
+	
+	@GetMapping()
+	public String info() {
+		return "well come to NR JAVA Training";
+	}
+
+}
